680a0dae3e09871f51e33cf1308fe67c6b1fff41
drabart <drabart@outlook.com>
2025-10-28 22:33:37 +0100

    updated with new version

--- src/robot_description/launch/display.launch.py
+++ src/robot_description/launch/display.launch.py
@@ -1,12 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_path = get_package_share_directory('robot_description')
+    urdf_path = os.path.join(pkg_path, 'urdf', 'qbert.urdf')
+
     rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', '$(find robot_description)/rviz/qbert_config.rviz'],
         output='screen'
     )
 
@@ -15,7 +19,7 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         parameters=[{'use_sim_time': False}],
-        arguments=['$(find robot_description)/urdf/qbert.urdf']
+        arguments=[urdf_path]
     )
 
     nodes = [
