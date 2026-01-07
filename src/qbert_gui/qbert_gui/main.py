4d2f195c155dcedf84df0b730a5676df2e703276
Bartosz Drabinski <drabart@outlook.com>
2026-01-07 22:08:40 +0100

    might work

--- src/qbert_gui/qbert_gui/main.py
+++ src/qbert_gui/qbert_gui/main.py
@@ -1,10 +1,60 @@
-import sys
-from PyQt6 import QtWidgets, uic, QtCore
-import os
+import os, sys
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool, String, Float64
+from PyQt6.QtCore import QObject, pyqtSignal, QtWidgets, uic, QtCore, QThread, pyqtSlot
+
+class RosWorker(QObject):
+    message_received = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        rclpy.init()
+        self.node = Node("pyqt_ros_node")
+        self.publishers = {}
+
+    def get_publisher(self, topic, msg_type):
+        if topic not in self.publishers:
+            self.publishers[topic] = self.node.create_publisher(
+                msg_type, topic, 10
+            )
+        return self.publishers[topic]
+    
+    @pyqtSlot(str, bool)
+    def publish_bool(self, topic, value):
+        msg = Bool()
+        msg.data = value
+        self.get_publisher(topic, Bool).publish(msg)
+
+    @pyqtSlot(str, float)
+    def publish_float(self, topic, value):
+        msg = Float64()
+        msg.data = value
+        self.get_publisher(topic, Float64).publish(msg)
+
+    @pyqtSlot(str, str)
+    def publish_float(self, topic, value):
+        msg = String()
+        msg.data = value
+        self.get_publisher(topic, String).publish(msg)
+
+    def callback(self, msg):
+        self.message_received.emit(msg.data)
+
+    def spin(self):
+        rclpy.spin(self.node)
+
+    def shutdown(self):
+        self.node.destroy_node()
+        rclpy.shutdown()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.load_ui()
+        self.setup_ros_worker()
+
+    def load_ui(self):
         self.setWindowTitle("Multi Screen App")
 
         # self.load_language(os.path.join(os.getcwd(), "i18n/nl.ts"))
@@ -34,6 +84,15 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Load default theme and language
         self.load_theme("themes/dark.qss")
+
+    def setup_ros_worker(self):
+        ros_thread = QThread()
+        ros_worker = RosWorker()
+
+        ros_worker.moveToThread(ros_thread)
+        ros_thread.started.connect(ros_worker.spin)
+
+        ros_thread.start()
 
     # Navigation
     def go_home(self):
@@ -70,4 +129,5 @@ if __name__ == "__main__":
     w = MainWindow()
     w.resize(400, 300)
     w.show()
-    sys.exit(app.exec())
+    exit_code = app.exec()
+    sys.exit(exit_code)
--- src/qbert_gui/setup.py
+++ src/qbert_gui/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'gui = qbert_gui.main:main'
         ],
     },
 )
