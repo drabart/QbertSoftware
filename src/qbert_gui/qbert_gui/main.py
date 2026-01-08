8ac9d711568ce7f299da8f53eead13580ad816cf
drabart <drabart@outlook.com>
2026-01-08 16:02:27 +0100

    made gui integrate with flexbe

--- src/qbert_gui/qbert_gui/main.py
+++ src/qbert_gui/qbert_gui/main.py
@@ -1,8 +1,11 @@
 import os, sys
 import rclpy
+from pathlib import Path
 from rclpy.node import Node
-from std_msgs.msg import Bool, String, Float64
-from PyQt6.QtCore import QObject, pyqtSignal, QtWidgets, uic, QtCore, QThread, pyqtSlot
+from std_msgs.msg import Bool, String, Float64, Empty
+from PyQt6 import QtWidgets, uic, QtCore
+from PyQt6.QtCore import QObject, pyqtSignal, QThread, pyqtSlot
+from ament_index_python.packages import get_package_share_directory
 
 class RosWorker(QObject):
     message_received = pyqtSignal(str)
@@ -13,12 +16,19 @@ class RosWorker(QObject):
         self.node = Node("pyqt_ros_node")
         self.publishers = {}
 
+        self.get_publisher("/gui_home", Empty)
+
     def get_publisher(self, topic, msg_type):
         if topic not in self.publishers:
             self.publishers[topic] = self.node.create_publisher(
                 msg_type, topic, 10
             )
         return self.publishers[topic]
+    
+    @pyqtSlot(str)
+    def publish_empty(self, topic):
+        msg = Empty()
+        self.get_publisher(topic, Empty).publish(msg)
     
     @pyqtSlot(str, bool)
     def publish_bool(self, topic, value):
@@ -33,7 +43,7 @@ class RosWorker(QObject):
         self.get_publisher(topic, Float64).publish(msg)
 
     @pyqtSlot(str, str)
-    def publish_float(self, topic, value):
+    def publish_string(self, topic, value):
         msg = String()
         msg.data = value
         self.get_publisher(topic, String).publish(msg)
@@ -48,51 +58,56 @@ class RosWorker(QObject):
         self.node.destroy_node()
         rclpy.shutdown()
 
+SHARE_DIR = Path(get_package_share_directory("qbert_gui"))
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.load_ui()
         self.setup_ros_worker()
 
+        self.home.homingButton.clicked.connect(lambda: self.ros_worker.publish_empty("/gui_home"))
+
     def load_ui(self):
         self.setWindowTitle("Multi Screen App")
 
-        # self.load_language(os.path.join(os.getcwd(), "i18n/nl.ts"))
-        self.load_language(os.path.join("i18n", "nl.qm"))
+        self.load_language(SHARE_DIR / "i18n" / "nl.qm")
 
-        # Stacked layout for screens
         self.stack = QtWidgets.QStackedWidget()
         self.setCentralWidget(self.stack)
 
-        # Load screens from XML (.ui)
-        self.home = uic.loadUi("ui/home.ui")
-        self.settings = uic.loadUi("ui/settings.ui")
-        self.debug = uic.loadUi("ui/debug.ui")
+        self.home = uic.loadUi(SHARE_DIR / "ui" / "home.ui")
+        self.settings = uic.loadUi(SHARE_DIR / "ui" / "settings.ui")
+        self.debug = uic.loadUi(SHARE_DIR / "ui" / "debug.ui")
 
-        # Add to stack
-        self.stack.addWidget(self.home)      # index 0
-        self.stack.addWidget(self.settings)  # index 1
-        self.stack.addWidget(self.debug)     # index 2
+        self.stack.addWidget(self.home)
+        self.stack.addWidget(self.settings)
+        self.stack.addWidget(self.debug)
 
-        # Connect navigation buttons
         self.home.settingsButton.clicked.connect(self.go_settings)
-
         self.settings.backButton.clicked.connect(self.go_home)
         self.settings.debugButton.clicked.connect(self.go_debug)
-
         self.debug.backButton.clicked.connect(self.go_settings)
 
-        # Load default theme and language
-        self.load_theme("themes/dark.qss")
+        self.load_theme(SHARE_DIR / "themes" / "dark.qss")
 
-    def setup_ros_worker(self):
-        ros_thread = QThread()
-        ros_worker = RosWorker()
+    def setup_ros_worker(self): 
+        self.ros_thread = QThread(self) 
+        self.ros_worker = RosWorker() 
 
-        ros_worker.moveToThread(ros_thread)
-        ros_thread.started.connect(ros_worker.spin)
+        self.ros_worker.moveToThread(self.ros_thread) 
+        self.ros_thread.started.connect(self.ros_worker.spin) 
 
-        ros_thread.start()
+        self.ros_thread.start()
+
+    def load_theme(self, path):
+        with open(SHARE_DIR / "themes" / "common.qss", "r") as f:
+            common_style = f.read()
+
+        with open(path, "r") as f:
+            theme_style = f.read()
+
+        app.setStyleSheet(common_style + "\n" + theme_style)
 
     # Navigation
     def go_home(self):
@@ -104,30 +119,27 @@ class MainWindow(QtWidgets.QMainWindow):
     def go_debug(self):
         self.stack.setCurrentIndex(2)
 
-    # Theme loading
-    def load_theme(self, path):
-        with open("themes/common.qss") as f:
-            common_style = f.read()
-
-        with open(path, "r") as f:
-            theme_style = f.read()
-
-        app.setStyleSheet(common_style + "\n" + theme_style)
-
-    # Language loading
     def load_language(self, path):
         if not hasattr(self, "translator"):
             self.translator = QtCore.QTranslator()
 
-        if self.translator.load(path):
+        if self.translator.load(str(path)):
             QtWidgets.QApplication.instance().installTranslator(self.translator)
         else:
             print("Failed to load:", path)
 
-if __name__ == "__main__":
+
+def main():
+    global app
+
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
     w.resize(400, 300)
     w.show()
     exit_code = app.exec()
     sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    main()
+    
