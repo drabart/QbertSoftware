bac948e2cf6c7d82a9db80bf87b777db98398a5b
drabart <drabart@outlook.com>
2026-01-18 14:43:10 +0100

    added popup to module

--- src/qbert_gui/qbert_gui/popups/__init__.py
+++ src/qbert_gui/qbert_gui/popups/__init__.py
@@ -2,5 +2,6 @@
 
 from .move_confirm_popup import ConfirmStartDialog
 from .offset_popup import PositionAdjustDialog
+from .continue_action_popup import ConfirmContinueDialog
 
-__all__ = ['ConfirmStartDialog', 'PositionAdjustDialog']
+__all__ = ['ConfirmStartDialog', 'PositionAdjustDialog', 'ConfirmContinueDialog']
